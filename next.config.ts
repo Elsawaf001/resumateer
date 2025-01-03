715a860e922014bf02e770622dba8f683b893660
ahmed elsawaf <71516525+Elsawaf001@users.noreply.github.com>
2025-01-03 10:22:48 +0200

    config fix

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "4mb",
+    },
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "f5mexfoh9g6whswf.public.blob.vercel-storage.com"
+      }
+    ]
+  } ,
   typescript : {ignoreBuildErrors : true} ,
-  eslint : {ignoreDuringBuilds : true} 
+  eslint : {ignoreDuringBuilds : true},
+  reactStrictMode: false ,
 };
+
 
 export default nextConfig;
