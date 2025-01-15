83f96630abc16c09e30eb24cc4e43a1d097f990a
elsawaf001 <elsawaf001@gmail.com>
2025-01-15 18:34:15 +0000

    test3

--- src/app/paid/[id]/page.tsx
+++ src/app/paid/[id]/page.tsx
@@ -3,15 +3,32 @@
 import React, { use, useEffect } from 'react'
 import { addPaidPoints } from './actions';
 import { useRouter } from 'next/navigation';
+import prisma from '@/lib/prisma';
 
 type Props = { params: Promise<{ id: string }> }
 
 async function Page(props: Props) {
-
+    const params = await props.params;
+    const userId = params.id ;
 const router = useRouter()
 
 useEffect( ()=> {
-      addPaidPoints(10000)
+    const existingSubscription =  prisma.userSubscription.findUnique({
+        where: { userId },
+      });
+
+      if (!existingSubscription) {
+       return
+    }
+
+   prisma.userSubscription.update({
+        where: { userId },
+        data: {
+          appPoints: { increment: 10000 },
+
+        },
+      });
+
      router.push("/resumes")
 },[])
    
