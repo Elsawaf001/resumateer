1342210aa60e8d6d580f5d59f7d7a5e53bb49ee1
elsawaf001 <elsawaf001@gmail.com>
2025-01-22 12:12:58 +0000

    try3

--- src/app/api/paddle-webhook/route.ts
+++ src/app/api/paddle-webhook/route.ts
@@ -2,19 +2,19 @@ import prisma from "@/lib/prisma";
 import { currentUser } from "@clerk/nextjs/server";
 import { Environment, EventName, SubscriptionCreatedEvent, SubscriptionUpdatedEvent } from "@paddle/paddle-node-sdk";
 import { Paddle } from "@paddle/paddle-node-sdk";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-const paddle = new Paddle(process.env.PADDLE_SECRET_TOKEN!, {
+const paddle = new Paddle("fec94087a2c6f8bf35aefaee14e9bbd9fc0cd46361aded0cca", {
   environment: Environment.sandbox,
 });
 
-export async function POST(req: Request) {
+export async function POST(req: NextRequest) {
   const signature = (req.headers.get("paddle-signature") as string) || "";
   // req.body should be of type `buffer`, convert to string before passing it to `unmarshal`.
   // If express returned a JSON, remove any other middleware that might have processed raw request to object
   const rawRequestBody = (await req.text()) || "";
   // Replace `WEBHOOK_SECRET_KEY` with the secret key in notifications from vendor dashboard
-  const secretKey = process.env.WEBHOOK_SECRET_KEY || "";
+  const secretKey = "pdl_ntfset_01jj2bwe5mty43vgv47w8z237j_EBNtCSrC7nkKiH7p7hAJ1MSxftmChSLx";
 
   try {
     if (signature && rawRequestBody) {
