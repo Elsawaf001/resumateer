4c84d0149ec89776b13e50b9eb02a4570b18ad4a
elsawaf001 <elsawaf001@gmail.com>
2025-01-23 12:59:09 +0000

    try4

--- src/components/premuim/paddle/FreeCard.tsx
+++ src/components/premuim/paddle/FreeCard.tsx
@@ -15,7 +15,7 @@ async function FreeCard() {
         return null;
     }
 
-    const subscription = await prisma.userSubscription.findUnique({
+    const freeSubscription = await prisma.userSubscription.findUnique({
         where: { userId },
         select: {
             appPoints: true,
@@ -23,16 +23,46 @@ async function FreeCard() {
         }
     });
 
-    return (
-        <Card className="mx-auto w-full max-w-7xl space-y-6 px-3 py-6">
-            <CardHeader className="text-2xl font-bold">You Are on <span className='text-lime-400'>Free</span>Plan</CardHeader>
-            <CardContent>Consumed Points <span className='text-lime-400 text-2xl font-bold'>{subscription?.appPoints}</span></CardContent>
+    const paidSubscription = await prisma.paddleCustomer.findUnique({
+        where: { userId },
+        select: {
 
-            <CardFooter>Avaliable Points <span className='text-lime-400 text-2xl font-bold'>{subscription?.userPoints}</span></CardFooter>
-            <Separator />
-            <h3 className='font-lg text-lime-400 text-center'>Want UnLimited Access ?</h3>
-            <MonthlyButton userId={userId} />
-        </Card>
+                paddlePriceId : true ,
+                paddleSubscriptionPeriodStart : true ,
+        }
+    });
+    const startDate = new Date(paidSubscription?.paddleSubscriptionPeriodStart!);
+    const paddleSubscriptionPeriodEnd = new Date(startDate);
+
+
+    return (
+        <>
+        {paddleSubscriptionPeriodEnd > new Date() && 
+        
+        <Card className="mx-auto w-full space-y-6 px-3 py-6">
+        <CardHeader className="text-lg font-bold">You Are on <span className='text-lime-400'>Premium</span>Plan</CardHeader>
+        <CardContent>Subscription start at<span className='text-lime-400  font-bold'>{formatDate(paidSubscription?.paddleSubscriptionPeriodStart!, "MMMM dd, yyyy")}</span></CardContent>
+
+        <CardFooter>Subscription Valid To <span className='text-lime-400  font-bold'>{paddleSubscriptionPeriodEnd.setDate(startDate.getDate() + 30)}</span></CardFooter>
+        {/* <Separator /> */}
+        {/* <h3 className='font-lg text-lime-400 text-center'></h3>
+        <MonthlyButton userId={userId} /> */}
+    </Card>
+        }
+        {paddleSubscriptionPeriodEnd > new Date() || !paidSubscription && 
+                <Card className="mx-auto w-full  space-y-6 px-3 py-6">
+                <CardHeader className="text-lg ">You Are on <span className='text-lime-400 font-bold'>Free</span>Plan</CardHeader>
+                <CardContent>Consumed Points <span className='text-lime-400 font-bold'>{freeSubscription?.appPoints}</span></CardContent>
+    
+                <CardFooter>Avaliable Points <span className='text-lime-400 font-bold'>{freeSubscription?.userPoints}</span></CardFooter>
+                <Separator />
+                <h6 className='font-sm text-lime-400 text-center'>Want UnLimited Access ?</h6>
+                <MonthlyButton userId={userId} />
+            </Card>
+        }
+
+
+     </>
     )
 }
 
