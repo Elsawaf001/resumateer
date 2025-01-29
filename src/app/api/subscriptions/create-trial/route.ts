66fee9e77cd7486faa7b571083843ac2b5a8a0bc
elsawaf001 <elsawaf001@gmail.com>
2025-01-29 16:37:35 +0000

    try

--- src/app/api/subscriptions/create-trial/route.ts
+++ src/app/api/subscriptions/create-trial/route.ts
@@ -1,25 +1,37 @@
 
 import prisma from '@/lib/prisma'
-import { auth } from '@clerk/nextjs/server'
+import { auth, currentUser } from '@clerk/nextjs/server'
 import { NextResponse } from 'next/server'
 
 export async function POST() {
   try {
     const {userId}  = await auth();
+    const userData= await currentUser();
+
     if (!userId) {
       return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
     }
     
+    const userExists = await prisma.user.findUnique({
+      where: { clerkUserId: userId },
+     
+    })
+    
+    if (!userExists) {
+      await prisma.user.create({
+        data: { clerkUserId: userId , 
+          email : userData?.emailAddresses[0].emailAddress!
+         }
+      })
+      // return NextResponse.json({ error: 'User not found' }, { status: 404 })
+    }
+
     const user = await prisma.user.findUnique({
       where: { clerkUserId: userId },
       include: { subscription: true }
     })
     
-    if (!user) {
-      return NextResponse.json({ error: 'User not found' }, { status: 404 })
-    }
-    
-    if (user.subscription) {
+    if (user?.subscription) {
       return NextResponse.json(
         { error: 'User already has a subscription' },
         { status: 400 }
@@ -33,7 +45,7 @@ export async function POST() {
       
       const subscription = await prisma.subscription.create({
         data: {
-          userId: user.id,
+          userId: user ? user.id : '',
           status: 'TRIALING',
           trialStart,
           trialEnd,
